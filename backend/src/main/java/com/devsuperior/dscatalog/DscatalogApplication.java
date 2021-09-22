6e0ceab5f3542ee460e54b52b9c03544556a124f
Pedro Lima <pedrojppb@gmail.com>
2021-09-22 15:32:34 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -1,30 +1,19 @@
 package com.devsuperior.dscatalog;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
 @SpringBootApplication
-public class DscatalogApplication implements CommandLineRunner {
-	
+public class DscatalogApplication {
+
 	@Autowired
 	CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		
-		Category category = new Category("Books");
-		categoryRepository.save(category);
-		
-		
 	}
 
 }
