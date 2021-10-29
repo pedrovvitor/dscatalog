89ff7f5ad11455ca6c11f6985ea4f53ff8ca01e8
Pedro Lima <pedrojppb@gmail.com>
2021-10-29 14:17:28 -0300

    ORM mapping and database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Role.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Role.java
@@ -1,6 +1,15 @@
 package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,6 +17,8 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@Entity
+@Table(name = "tb_role")
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,8 +27,20 @@ public @Data class Role implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String authority;
+	
+	@ManyToMany(mappedBy = "roles")
+	public Set<User> users;
+	
+	public Set<User> getUsers() {
+		if(users == null) {
+			return new HashSet<>();
+		}
+		return users;
+	}
 	
 	
 	
--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -4,12 +4,23 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@Entity
+@Table(name = "tb_user")
 @SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,12 +29,18 @@ public @Data class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String password;
 	
+	@ManyToMany
+	@JoinTable(name = "tb_user_role",
+	joinColumns = @JoinColumn(name = "user_id"),
+	inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles;
 	
 	public Set<Role> getRoles() {
