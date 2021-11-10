47003d10512b6e8a7c904973d035bba972d19796
Pedro Lima <pedrojppb@gmail.com>
2021-11-10 16:29:40 -0300

    Custom validation response

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/FieldMessage.java
@@ -0,0 +1,17 @@
+package com.devsuperior.dscatalog.resources.exceptions;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FieldMessage implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private String fieldName;
+	private String message;
+}
